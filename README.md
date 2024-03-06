3392f497b892ba558732dbdb3f69cef085ca6eaf
Nicolas <nicart@gmail.com>
2024-03-06 15:59:09 +0000

    From refs/heads/main 52192e4bc0a85eeea8c58d2f634c3cc9019ab7a7

---
+++ README.md
@@ -0,0 +1,3 @@
+#GitHub Pages
+
+Last update of sphinx html documentation from [52192e4](https://github.com/earthdaily/earthdaily-python-client/tree/52192e4bc0a85eeea8c58d2f634c3cc9019ab7a7)
---
+++ _downloads/b09ec3908232297d6b52ba5806a1f799/compare_scale_s2.py
@@ -0,0 +1,71 @@
+"""
+Compare Sentinel-2 scale/offset evolution during time
+=================================================================
+
+Using SCL data from L2A"""
+
+##############################################################################
+# Import librairies
+# -------------------------------------------
+
+from earthdaily import earthdatastore, datasets
+import geopandas as gpd
+from matplotlib import pyplot as plt
+
+##############################################################################
+# Load plot
+# -------------------------------------------
+
+# load geojson
+pivot = datasets.load_pivot()
+
+##############################################################################
+# Init earthdatastore with env params
+# -------------------------------------------
+
+eds = earthdatastore.Auth()
+
+##############################################################################
+# Search for collection items
+#
+def get_cube(rescale=True):
+    pivot_cube = eds.datacube(
+        "sentinel-2-l2a",
+        intersects=pivot,
+        datetime=["2022-01-01", "2022-03-10"],
+        assets=["red", "green", "blue"],
+        mask_with="native",  # same as scl
+        clear_cover=50,  # at least 50% of the polygon must be clear
+        rescale=rescale)
+    return pivot_cube
+
+
+##############################################################################
+# Get cube with rescale (*0.0001)
+# ----------------------------------------------------
+
+pivot_cube = get_cube(rescale=False) * 0.0001
+
+##############################################################################
+# Plots cube with SCL with at least 50% of clear data
+# ----------------------------------------------------
+
+pivot_cube.ed.plot_rgb(col_wrap=3)
+plt.show()
+
+##############################################################################
+# Get cube with automatic rescale (default option)
+# ----------------------------------------------------
+
+pivot_cube = get_cube()
+pivot_cube.clear_percent.plot.scatter(x="time")
+plt.show()
+
+##############################################################################
+# Plots cube with SCL with at least 50% of clear data
+# ----------------------------------------------------
+
+
+pivot_cube.ed.plot_rgb(col_wrap=3)
+
+plt.show()
