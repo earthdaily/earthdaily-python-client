6adfc25c9d8268aa988062252d89d4eda3a8b9ad
nkarasiak <nkarasiak@users.noreply.github.com>
2024-06-05 12:34:25 +0000

    style(ruff) : automatic lint/format

--- earthdaily/earthdatastore/__init__.py
+++ earthdaily/earthdatastore/__init__.py
@@ -776,8 +776,7 @@ class Auth:
                     {grouped_coord.name: grouped_coord}
                 )
             xr_datacube["time"] = xr_datacube.time.astype("<M8[ns]")
-            
-                
+
         return xr_datacube
 
     def _update_search_for_assets(self, assets):
