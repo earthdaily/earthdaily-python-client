9c8324fabc3e7f35b175ad54508cf72ac8c1a9d5
nicolasK <nicolas.karasiak@earthdaily.com>
2024-06-19 14:20:48 +0200

    fix(example)

--- examples/field_evolution.py
+++ examples/field_evolution.py
@@ -25,21 +25,17 @@ pivot = datasets.load_pivot()
 
 eds = earthdatastore.Auth()
 
-wkt = "POLYGON ((-114.995866 52.045233, -114.993462 52.046183, -114.988397 52.04159, -114.991187 52.040745, -114.995866 52.045233))"
-
 ##############################################################################
 # Search for collection items in august 2022 (1st to 9th)
 # where at least 50% of the field is clear according to the native cloudmask.
 
 pivot_cube = eds.datacube(
     "sentinel-2-l2a",
-    intersects=wkt,
-    datetime="2020-06",
-    assets=["red", "green", "blue", "swir16"],
-    mask_with=["cloud_mask"],
+    intersects=pivot,
+    datetime=["2023-07-01","2023-07-31"],
+    assets=["red", "green", "blue"],
+    mask_with=["native"],
     clear_cover=50,
-    search_kwargs=dict(query={"eo:cloud_cover":{"lt":90}}),
-    chunks=dict(time=-1)
 )
 pivot_cube.clear_percent.plot.scatter(x="time")
 
@@ -47,26 +43,18 @@ pivot_cube.clear_percent.plot.scatter(x="time")
 # Plots cube with SCL with at least 50% of clear data
 # ----------------------------------------------------
 pivot_cube = pivot_cube.load()
-
-# Setting the background color of the plot 
-# using set_facecolor() method
-pivot_cube = pivot_cube.ed.add_indices(['NDSI'])
-test = pivot_cube.where(~(pivot_cube['NDSI']>=0))
-# test.ed.plot_rgb(col_wrap=3, white_background=False, vmin=0,vmax=0.2)
-pivot_cube.ed.plot_rgb(col_wrap=10, vmin=0,vmax=0.15, background=0)
-# test.ed.plot_rgb(col_wrap=10, white_background=False, vmin=0,vmax=0.15)
-
- 
-# plt.title("Pivot evolution masked with CNN cloudmask filtered with NDSI")
-# plt.show()
+pivot_cube.ed.plot_rgb(col_wrap=3)
+plt.title("Pivot evolution masked with native cloudmasks")
+plt.show()
 
 
-# ##############################################################################
-# # Compute zonal stats for the pivot
-# # ----------------------------------------------------
+##############################################################################
+# Compute zonal stats for the pivot
+# ----------------------------------------------------
 
-# zonal_stats = pivot_cube.ed.zonal_stats(pivot, ['mean','max','min'])
+zonal_stats = pivot_cube.ed.zonal_stats(pivot, ['mean','max','min'])
 
-# zonal_stats.isel(feature=0).to_array(dim="band").plot.line(
-#     x="time", col="band", hue="stats"
-# )
+zonal_stats.isel(feature=0).to_array(dim="band").plot.line(
+    x="time", col="band", hue="stats"
+)
+plt.show()
