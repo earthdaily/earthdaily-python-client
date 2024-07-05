dbf08102fd1de67ec7e4b2261471fc0dc68305b1
rle-earthdaily <rle-earthdaily@users.noreply.github.com>
2024-07-05 09:58:46 +0000

    style(ruff) : automatic lint/format

--- earthdaily/utils/copy_credentials_template.py
+++ earthdaily/utils/copy_credentials_template.py
@@ -8,13 +8,14 @@ import json
 import toml
 from pathlib import Path
 
-default_path = Path.home()/".earthdaily/credentials"
+default_path = Path.home() / ".earthdaily/credentials"
 
 default_configuration = {
-    "EDS_AUTH_URL" : "https://..",
-    "EDS_CLIENT_ID" : "123",
-    "EDS_SECRET" : "123",
+    "EDS_AUTH_URL": "https://..",
+    "EDS_CLIENT_ID": "123",
+    "EDS_SECRET": "123",
 }
+
 
 def write_json(json_path: Path) -> None:
     """
@@ -37,6 +38,7 @@ def write_json(json_path: Path) -> None:
     print(f"Credentials file written to {json_path}")
     print("Please edit it to insert your credentials")
 
+
 def write_toml(toml_path: Path) -> None:
     """
     Write template TOML credentials file.
@@ -53,10 +55,11 @@ def write_toml(toml_path: Path) -> None:
         return
 
     with toml_path.open("w") as f:
-        toml.dump({"default" : default_configuration}, f)
+        toml.dump({"default": default_configuration}, f)
 
     print(f"Credentials file written to {toml_path}")
     print("Please edit it to insert your credentials")
+
 
 def write_env(env_path: Path) -> None:
     """
@@ -83,24 +86,49 @@ def write_env(env_path: Path) -> None:
 
 
 @click.command("Copy credentials templates in all accepted formats")
-@click.option("--json", "json_path", type = click.Path(path_type = Path, exists = False), required = False, help = "Path to the output JSON file containing the credentials keys (but no values)")
-@click.option("--toml", "toml_path", type = click.Path(path_type = Path, exists = False), required = False, help = "Path to the output TOML file containing the credentials keys (but no values)")
-@click.option("--env", "env_path", type = click.Path(path_type = Path, exists = False), required = False, help = "Path to the output .env file containing the credentials keys (but no values)")
-@click.option("--default", "default", is_flag=True, show_default=True, default = False, help=f"Copy the TOML template to {default_path}, with credential keys (and no values)")
-def cli(json_path:Path, toml_path: Path, env_path: Path, default: bool) -> None:
-
+@click.option(
+    "--json",
+    "json_path",
+    type=click.Path(path_type=Path, exists=False),
+    required=False,
+    help="Path to the output JSON file containing the credentials keys (but no values)",
+)
+@click.option(
+    "--toml",
+    "toml_path",
+    type=click.Path(path_type=Path, exists=False),
+    required=False,
+    help="Path to the output TOML file containing the credentials keys (but no values)",
+)
+@click.option(
+    "--env",
+    "env_path",
+    type=click.Path(path_type=Path, exists=False),
+    required=False,
+    help="Path to the output .env file containing the credentials keys (but no values)",
+)
+@click.option(
+    "--default",
+    "default",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help=f"Copy the TOML template to {default_path}, with credential keys (and no values)",
+)
+def cli(json_path: Path, toml_path: Path, env_path: Path, default: bool) -> None:
     if json_path is not None:
-        write_json(json_path = json_path)        
+        write_json(json_path=json_path)
 
     if toml_path is not None:
-        write_toml(toml_path = toml_path)
+        write_toml(toml_path=toml_path)
 
     if env_path is not None:
-        write_env(env_path = env_path)
+        write_env(env_path=env_path)
 
     if default:
-        default_path.parent.mkdir(exist_ok = True, parents = True)
-        write_toml(toml_path = default_path)
-        
+        default_path.parent.mkdir(exist_ok=True, parents=True)
+        write_toml(toml_path=default_path)
+
+
 if __name__ == "__main__":
     cli()
