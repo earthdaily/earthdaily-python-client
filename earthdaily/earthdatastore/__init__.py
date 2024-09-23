d04aeb8d642939c92bf6f86f7e28c04f45702801
nicolasK <nicolas.karasiak@earthdaily.com>
2024-09-23 17:39:58 +0200

    fix(datacube) : search by bbox

--- earthdaily/earthdatastore/__init__.py
+++ earthdaily/earthdatastore/__init__.py
@@ -943,8 +943,7 @@ class Auth:
         # query the items
         items = self.search(
             collections=collections,
-            bbox=list(cube_utils.GeometryManager(intersects).to_bbox()),
-            # intersects=intersects,
+            bbox=list(cube_utils.GeometryManager(intersects).to_bbox()) if bbox is None else bbox,
             datetime=datetime,
             assets=assets,
             prefer_alternate=prefer_alternate,
@@ -989,10 +988,11 @@ class Auth:
             xr_datacube = xr_datacube.ed.clip(intersects)
         # Create mask datacube and apply it to xr_datacube
         if mask_with:
-            kwargs["dtype"] = "int8"
             if "geobox" not in kwargs:
                 kwargs["geobox"] = xr_datacube.odc.geobox
-
+            kwargs.pop('crs','')
+            kwargs.pop('resolution','')
+            kwargs["dtype"] = "int8"
             if clear_cover and mask_statistics is False:
                 mask_statistics = True
             mask_kwargs = dict(mask_statistics=False)
@@ -1025,10 +1025,6 @@ class Auth:
                     ]: mask._native_mask_def_mapping[collections[0]]
                 }
 
-                if "resolution" in kwargs:
-                    kwargs.pop("resolution")
-                if "epsg" in kwargs:
-                    kwargs.pop("epsg")
 
                 clouds_datacube = datacube(
                     items,
