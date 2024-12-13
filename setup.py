04d6990619c0d0f6160f9f14894506993d58b066
nicolasK <nicolas.karasiak@earthdaily.com>
2024-12-13 10:15:36 +0100

    requirements : matplotlib<3.9
    due to cmap

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "numpy",
-        "matplotlib",
+        "matplotlib<3.9",
         "joblib",
         "scipy",
         "psutil",
