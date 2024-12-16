bd1d7a54bfd0be9d58239ff21a63e811d58b181c
nicolasK <nicolas.karasiak@earthdaily.com>
2024-12-16 16:06:25 +0100

    fix(zonal_stats) : wkt

--- earthdaily/earthdatastore/cube_utils/_zonal.py
+++ earthdaily/earthdatastore/cube_utils/_zonal.py
@@ -212,8 +212,9 @@ def zonal_stats(
 
         if geoms.crs.to_epsg() != 4326:
             geoms = geoms.to_crs("EPSG:4326")
+        
         geometry = xr.DataArray(
-            geoms.iloc[list(f - 1)].geometry.apply(lambda x: x.wkt).values,
+            geoms.geometry.iloc[list(f-1)].to_wkt(rounding_precision=-1).values,
             dims=["feature"],
             coords={"feature": zs.feature.values},
         )
